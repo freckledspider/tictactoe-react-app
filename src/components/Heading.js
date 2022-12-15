2016e921e6e4311f20ec5d2816823c1654ab1af2
bri <bcamesi@gmail.com>
2022-12-14 21:46:52 -0500

    tictactoe intro to react app

---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+function Heading(props){
+
+    console.log(props)
+
+    const jsx = <h2>Goodbye World</h2>
+  
+    return <><h1>{props.cheese}</h1><h3>{props.bread}</h3></>
+  }
+
+export default Heading
+// same thing as module.exports = Heading
